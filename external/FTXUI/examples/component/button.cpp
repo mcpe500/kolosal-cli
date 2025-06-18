dcc5ebb553f406b74857f2ae3730fe003dbf38f7
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2025-06-18 23:53:02 +0700

    remove FTXUI

--- external/FTXUI/examples/component/button.cpp
+++
@@ -1,9 +0,0 @@
-#include "ftxui/component/component.hpp"
-#include "ftxui/component/screen_interactive.hpp"
-
-int main(){
-    auto screen = ftxui::ScreenInteractive::Fullscreen();
-    auto testComponent = ftxui::Renderer([](){return ftxui::text("test Component");});
-    screen.Loop(testComponent);
-    return 0;
-}
