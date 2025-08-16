7b6763a577ba2b27dcb8a7cb950f1c24775ba7d3
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2025-08-16 14:04:22 +0700

    feat: Update split_mode parameter to default to 0 in addEngine function

--- src/kolosal_server_client.cpp
+++ src/kolosal_server_client.cpp
@@ -565,7 +565,7 @@ bool KolosalServerClient::addEngine(const std::string &engineId, const std::stri
         loadParams["n_gpu_layers"] = 50;
         loadParams["n_batch"] = 2048;
         loadParams["n_ubatch"] = 512;
-        loadParams["split_mode"] = -1;
+        loadParams["split_mode"] = 0;
         
         payload["loading_parameters"] = loadParams;
 
